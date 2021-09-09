733e95d11703df57bfeb9f490e5503f9f121ec4e
Rahman Elsirafy <rahman2408@gmail.com>
2021-09-09 01:40:48 -0400

    commit tree 248464f5b36b681749d5e8d76b7ab8b9a81bd269

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let driver = {
+  name: "Rahman",
+  age: 26,
+  state: "New York"
+};
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+  return Object.assign({}, driver, {[key]: value});
+};
+
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+  return Object.assign(driver, { [key]: value });
+};
+
+const deleteFromDriverByKey= (driver, key) => {
+  let newDriver = Object.assign({},driver);
+  delete newDriver[`${key}`];
+  return newDriver;
+};
+
+const destructivelyDeleteFromDriverByKey= (driver, key) => {
+  delete driver[`${key}`];
+  return driver;
+};
